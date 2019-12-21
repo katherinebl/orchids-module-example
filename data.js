e47ea1534edea471a79bd54c7e639a5759096eb1
katherinebl <katherine.briceno@kairosds.com>
2019-12-21 19:00:08 +0100

    Added mediaqueries and finish styles

--- data.js
+++ data.js
@@ -1,39 +1,44 @@
 export const picsArr = [
     {
         id: 1,
-        name: "orchid 1",
-        route: "pics/orchid1.jpg"
+        name: "flower 1",
+        route: "pics/flower1.jpg"
     },
     {
         id: 2,
-        name: "orchid 2",
-        route: "pics/orchid2.jpg"
+        name: "flower 2",
+        route: "pics/flower2.jpg"
     },
     {
         id: 3,
-        name: "orchid 3",
-        route: "pics/orchid3.jpg"
+        name: "flower 3",
+        route: "pics/flower3.jpg"
     },
     {
         id: 4,
-        name: "orchid 4",
-        route: "pics/orchid4.jpg"
+        name: "flower 4",
+        route: "pics/flower4.jpg"
     },
     {
         id: 5,
-        name: "orchid 5",
-        route: "pics/orchid5.jpg"
+        name: "flower 5",
+        route: "pics/flower5.jpg"
     },
     {
         id: 6,
-        name: "orchid 6",
-        route: "pics/orchid6.jpg"
+        name: "flower 6",
+        route: "pics/flower6.jpg"
     },
     {
         id: 7,
-        name: "orchid 7",
-        route: "pics/orchid7.jpg"
+        name: "flower 7",
+        route: "pics/flower7.jpg"
     },
+    {
+        id: 8,
+        name: "flower 8",
+        route: "pics/flower8.jpg"
+    }
 
 ]
 
--- main.js
+++ main.js
@@ -3,20 +3,25 @@ import { picsArr, Orchid } from "./data.js";
 const container = document.querySelector(".container");
 
 picsArr.forEach(item => {
+    const card = document.createElement('div');
+    card.setAttribute('class', "card");
+
     const pic = document.createElement('img');
     pic.setAttribute('src', item.route);
-    container.appendChild(pic);
+    card.appendChild(pic);
 
-    const name = document.createElement('p');
+    const name = document.createElement('h2');
     name.textContent = item.name;
-    container.appendChild(name);
+    card.appendChild(name);
 
     const id = document.createElement('p');
     id.textContent = `This is the orchid number ${item.id}`;
-    container.appendChild(id);
+    card.appendChild(id);
+
+    container.appendChild(card);
 })
 
-const orchid = new Orchid('Orchid number');
+const orchid = new Orchid('ENJOY THESE FLOWERS');
 document.querySelector('.main__title').innerHTML = orchid.presentation();
 
 
